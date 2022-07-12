77d3ceb2508069dfc7f67ba8b1ec8367879189cd
Dmitry Migunov <d.migunovs@bilderlings.com>
2022-07-12 16:10:15 +0300

    IMAP plugin

---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :imap_authenticate do
+  name 'IMAP authenticate plugin'
+  author 'Bilderlings'
+  description 'Uses IMAP authenticate method instead of login'
+  version '0.1.0'
+end
